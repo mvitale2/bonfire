6044ceba406971862d30f507f8376129d08e96af
skel101 <197417960+skel101@users.noreply.github.com>
2025-07-01 20:05:12 -0400

    fix useEffect error

--- src/renderer/src/components/Pages/UserSettings/UserSettings.jsx
+++ src/renderer/src/components/Pages/UserSettings/UserSettings.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./UserSettings.css";
 import { UserContext } from "../../../UserContext.jsx";
