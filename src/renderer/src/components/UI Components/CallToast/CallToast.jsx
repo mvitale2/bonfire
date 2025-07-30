6194f1ae7b6a6ec441e39a1ad691ee5d12c7b992
skel101 <197417960+skel101@users.noreply.github.com>
2025-07-29 20:05:32 -0400

    sync

--- src/renderer/src/components/UI Components/CallToast/CallToast.jsx
+++ src/renderer/src/components/UI Components/CallToast/CallToast.jsx
@@ -21,7 +21,7 @@ function CallToast({
 
   // peer creation for receiver & initiator
   useEffect(() => {
-    if (inCall === false) return;
+    if (inCall === false || callAccepted === false) return;
 
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
       const localPeer = new SimplePeer({
@@ -83,7 +83,7 @@ function CallToast({
 
         return () => supabase.removeChannel(subscription);
     });
-  }, [inCall]);
+  }, [inCall, callAccepted]);
 
   useEffect(() => {
     const fetchNickname = async () => {
